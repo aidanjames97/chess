1408c19d4caca2000cf8804ce22871732aec83ad
Aidan James <ajame23@uwo.ca>
2024-03-18 21:57:10 -0400

    fixed alternate move bug

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QLabel>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv); // init QT application
     MainWindow w; // window object
+
     w.setWindowTitle("Chess"); // title of window
-    w.setFixedSize(800, 800);
+    w.setFixedSize(1000, 800);
     w.setStyleSheet("background-color: #C0C0C0");
     w.show(); // show window
     return a.exec(); // run app
