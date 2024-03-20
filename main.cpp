98cc058bcbddd13fcb19408733279a80a69b6c46
Aidan James <ajame23@uwo.ca>
2024-03-20 16:58:50 -0400

    functioning game, going to fix qlayout problem

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <QLabel>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
+    cout << "\n=====\n" << endl;
+
     QApplication a(argc, argv); // init QT application
+
+    cout << " -=-=-=-=-=" << endl;
+
     MainWindow w; // window object
+
+    cout << "\n=====\n" << endl;
 
     w.setWindowTitle("Chess"); // title of window
     w.setFixedSize(800, 800);
